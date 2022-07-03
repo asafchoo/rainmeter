f353d6d41ab62054b8d0d701d04af43fac7a320b
asafchoo <45887109+asafchoo@users.noreply.github.com>
2022-07-04 00:09:05 +0300

    Update and rename 3.usensor-filer-by-10-and-take-avrage.ino to 3:usensor-filer-by-10-and-take-avrage.ino

--- 3:usensor-filer-by-10-and-take-avrage.ino
+++ 3:usensor-filer-by-10-and-take-avrage.ino
@@ -1,5 +1,5 @@
-#define echoPin 4 // הפין אקו (Echo) של החיישן האולטרסוני
-#define trigPin 5 //הפין טריג (trig) של החיישן האולטרסוני
+#define trigPin 4 //הפין טריג (trig) של החיישן האולטרסוני
+#define echoPin 5 // הפין אקו (Echo) של החיישן האולטרסוני
 
 float duration; // variable for the duration of sound wave travel
 float mm; // variable for the distance measurement
@@ -7,36 +7,42 @@ float mm; // variable for the distance measurement
 float measurements[] = {};
 
 void setup() {
-  // put your setup code here, to run once:
- Serial.begin(115200);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+   Serial.begin(115200);
+   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
 }
 
 void loop() {
   for (int count = 0; count < 10; count++) {
     digitalWrite(trigPin, LOW);
-    delay(100);             
+    delay(500);         
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);
+
     mm = (duration * 0.034 / 2) * 10; // Speed of sound wave divided by 2 (go and back)
     measurements[count] = mm;
   }
+
   calculateavrage(10);
+
 }
 
 float calculatefrequent(float a[], int size) {
   int most = 0;     /*- כמות הפעמים שמופיע המספר שחוזר על עצמו הכי הרבה פעמים -*/
   float mostnum = 0;       /*- המספר שחוזר על עצמו הכי הרבה פעמים -*/
   float currentnum = 0;       /*- המספר שהפונקציה "חוקרת" כרגע  -*/ 
-  for (int i=0; i <size; i++) {        /*- עבור כל i  -*/   
+  for (int i=0; i <size; i++) {        /*- עבור כל i  -*/
+      delay(200);             
       currentnum=a[i];             /*- המספר שנחקר כרגע הוא הi במערך -*/
+      Serial.print(i+1);
+      Serial.print(": ");
+      Serial.println(currentnum);
       int count = 0;                 /*- משתנה שסופר כמה פעמים הוא מופיע -*/
       for (int j=0;j <size; j++) {          /*- עבור כל j -*/
         if ( a[i] != mostnum) {                  /*- אם אנו חוקרים מספר שונה מהגבוה ביותר -*/
-          if (currentnum == a[j]) count += 1;            /*- אז אם המספר הנחקר שווה לזה שמופיע במערך במיקום j ספור +1 -*/
+          if (currentnum == a[j]) count += 1;            /*- אז אם המספר הנחקר שווה לזה שמופיע במערך במיקום j ספור +1 -*/   
         }
       }
       if (count > most) {                     /*- אם המספר הנחקר מופיע יותר פעמים מקודמיו -*/
@@ -44,12 +50,13 @@ float calculatefrequent(float a[], int size) {
         mostnum = a[i];                      /*- וזה המספר החדש -*/
       }
   }
+  delay(2000);
   Serial.print("Most Frequent: ");            /*- אחרי שנעשה את זה עשר פעמים -*/
   Serial.println(mostnum);  /*- המספר שמופיע הכי הרבה פעמים נמצא במשתנה הזה -*/
   return mostnum;
 }
 
-loat calculateavrage(int size) {
+float calculateavrage(int size) {
   float avrge;
   for (int i=0; i <size; i++) {        /*- עבור כל i  -*/
       float val = calculatefrequent(measurements,10);
